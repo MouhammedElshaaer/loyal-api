2a8cf614c0f6cdd1b345e6c735af1ff33ccfa43d
MouhammedElshaaer <mouhammedelshaaer@stud.fci-cu.edu.eg>
2019-07-31 21:01:27 +0200

    headers modification

--- app/Http/Controllers/Web/AdminController.php
+++ app/Http/Controllers/Web/AdminController.php
@@ -273,13 +273,7 @@ class AdminController extends Controller
     public function getActionLogs(Request $request){
         
         $this->initResponse(200, 'success', ActionLogResource::collection($this->getAllDataRows(ActionLog::class)));
-        return response()->json($this->data, 200)
-                        ->header('Access-Control-Allow-Origin', '*')
-                        ->header('Vary', 'Origin')
-                        ->header('Access-Control-Allow-Credentials', 'false')
-                        ->header('Access-Control-Allow-Methods', '*')
-                        ->header('Access-Control-Allow-Headers', '*');
-                        // ->header('Access-Control-Allow-Origin', \URL::to('/'));
+        return response()->json($this->data, 200);
     }
 
     /*******************************************************************************
