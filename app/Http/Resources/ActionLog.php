70b998d1dae178bf8247427e01f744db190302c1
MouhammedElshaaer <mouhammedelshaaer@stud.fci-cu.edu.eg>
2019-08-27 01:41:10 +0200

    adding voucherInstance details to ActionLogResource

--- app/Http/Resources/ActionLog.php
+++ app/Http/Resources/ActionLog.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 use App\Http\Resources\User as UserResource;
+use App\Http\Resources\VoucherInstance as VoucherInstanceResource;
 
 use App\Http\Traits\CRUDUtilities;
 use App\Http\Traits\StatusUtilities;
@@ -39,14 +40,13 @@ class ActionLog extends JsonResource
 
             $voucherInstance = null;
             if ($this->action->type == config('constants.actions.transaction_voucher_used_success')){
-                $voucherInstance = $dataRow->voucherInstance->id;
+                $voucherInstance = $dataRow->voucherInstance;
             }
 
-            // $attributes['customer'] = $dataRow->user? $dataRow->user->name: null;
             $attributes['customer'] = $dataRow->user? new UserResource($dataRow->user): null;
             $attributes['invoice_number'] = $dataRow->invoice_number;
             $attributes['invoice_value'] = $dataRow->invoice_value;
-            $attributes['voucher_id'] = $voucherInstance;
+            $attributes['voucher'] = $voucherInstance;
             $attributes['points'] = $dataRow->transactionPoints->original;
             $attributes['status'] = $status;
             $attributes['action'] = $this->action->type;
@@ -55,11 +55,10 @@ class ActionLog extends JsonResource
 
             $statusCode = $dataRow->status;
 
-            // $attributes['customer'] = $this->scope->name != config("constants.scopes.cashier")? $user->name: null;
             $attributes['customer'] = $this->scope->name != config("constants.scopes.cashier")? new UserResource($dataRow->user): null;
             $attributes['invoice_number'] = null;
             $attributes['invoice_value'] = null;
-            $attributes['voucher_id'] = null;
+            $attributes['voucher'] = new VoucherInstanceResource($dataRow);
             $attributes['points'] = $dataRow->voucher->points;
             $attributes['status'] = $this->resolveStatusFromStatusCode($statusCode);
             $attributes['action'] = $this->action->type;
@@ -67,13 +66,11 @@ class ActionLog extends JsonResource
 
         return [
             'id' => $this->id,
-            // 'cashier' => $this->when($this->scope->name == config("constants.scopes.cashier"), $user->name, null),
             'cashier' => $this->when($this->scope->name == config("constants.scopes.cashier"), new UserResource($dataRow->user), null),
             'customer' => $attributes['customer'],
             'invoice_number' => $this->when(array_key_exists('invoice_number', $attributes), $attributes['invoice_number'], null),
             'invoice_value' => $this->when(array_key_exists('invoice_value', $attributes), $attributes['invoice_value'], null),
-            // 'voucher' => $this->when(array_key_exists('voucher_id', $attributes), $attributes['voucher_id'], null),
-            // 'voucher_id' => $this->when(array_key_exists('voucher_id', $attributes), $attributes['voucher_id'], null),
+            'voucher' => $this->when(array_key_exists('voucher', $attributes), new VoucherInstanceResource($attributes['voucher']), null),
             'points' => $this->when(array_key_exists('points', $attributes), $attributes['points'], null),
             'status' => $this->when(array_key_exists('status', $attributes), $attributes['status'], null),
             'action' => $this->when(array_key_exists('action', $attributes), $attributes['action'], null),
